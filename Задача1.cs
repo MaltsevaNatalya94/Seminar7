1392f97a62d5eaf15be5bfef74c813d0c8b68db1
MaltsevaNatalya94 <118058268+MaltsevaNatalya94@users.noreply.github.com>
2022-12-01 00:02:44 +0300

    Add files via upload

---
+++ Задача1.cs
@@ -0,0 +1,23 @@
+﻿// Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+Random rnd = new Random();
+
+for (int i = 0; i < m; i++)
+ { 
+    for (int j = 0; j < n; j++)
+    { 
+        array[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);
+    }
+ }
+ for (int i = 0; i < m; i++)
+ { 
+    for (int j = 0; j < n; j++)
+    { 
+        Console.Write($"{array[i, j]} ");
+    }
+ }
---
+++ Задача3.cs
@@ -0,0 +1,42 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.WriteLine("Задайте количество строк двумерного массива:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество столбцов двумерного массива:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [m, n];
+Random rnd = new Random();
+int sum=0;
+double sred = 0;
+
+for (int i = 0; i < m; i++)
+ { 
+    for (int j = 0; j < n; j++)
+    { 
+        array[i,j] = Convert.ToInt32(rnd.Next(1, 10));
+    }
+ }
+ for (int i = 0; i < m; i++)
+ { 
+    for (int j = 0; j < n; j++)
+    { 
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+ }
+    Console.WriteLine();
+for (int j = 0; j < array.GetLength(1); j ++)
+{
+    sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum  += array [i, j];
+    }
+    sred=sum / array.GetLength(0);
+    Console.Write($"{sred} ");
+}
